51121c0727ad198c183b3cb435b91ba13ed7dfac
toilaloc <locpc.qb@gmail.com>
2020-10-31 09:09:27 +0700

    this project for a special person

---
+++ app.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    var flame = $("#flame");
+    var txt = $("h1");
+  
+    $('#flame').on('click', function () {
+        flame.removeClass("burn").addClass("puff");
+        $(".smoke").each(function () {
+          $(this).addClass("puff-bubble");
+        });
+        $("#glow").remove();
+        txt.hide().html("It <b>will</b> come true..").delay(750).fadeIn(300);
+        $('#letter').removeClass("hide").removeAttr("class");
+        $("#candle").animate(
+          {
+            opacity: ".5"
+          },
+          100
+        );
+      }
+    );
+});
+
+  
